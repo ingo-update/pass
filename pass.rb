6acbf578e15eee9a888dac9839b725873106689d
Ingemar Åberg <ingo@update.uu.se>
2021-04-22 13:49:28 +0200

    BUG: Fix cut/paste error in ARGV handling

--- pass.rb
+++ pass.rb
@@ -8,10 +8,10 @@ def generate_pass(length, digits, caps, syms)
 
   rnd = Random.new(Time.now.to_i)
 
-  rest.times   { pass << (97 + rnd.rand(26)) }
   digits.times { pass << rnd.rand(10).to_s }
-  caps.times   { pass << (65 + rnd.rand(26)) }
   syms.times   { pass << symbols.sample }
+  caps.times   { pass << (65 + rnd.rand(26)) }
+  rest.times   { pass << (97 + rnd.rand(26)) }
 
   pass.chars.shuffle.join
 end
@@ -19,6 +19,6 @@ end
 length = (ARGV[0] ? ARGV[0].to_i : 16)
 digits = (ARGV[1] ? ARGV[1].to_i : 2)
 caps =   (ARGV[2] ? ARGV[2].to_i : 2)
-syms =   (ARGV[2] ? ARGV[3].to_i : 2)
+syms =   (ARGV[3] ? ARGV[3].to_i : 2)
 
 10.times { puts generate_pass(length, digits, caps, syms) }
